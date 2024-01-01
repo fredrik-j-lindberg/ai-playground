64c7f77d38e74ff018dfe8a34694133004cab994
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2024-01-01 13:42:27 +0100

    chore: improved api setup

--- src/ai/openai.ts
+++ src/ai/openai.ts
@@ -2,13 +2,13 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export const chatCompletion = async (prompt: string) => {
+export const promptAi = async (prompt: string) => {
   try {
     const completion = await openai.chat.completions.create({
       messages: [{ role: "system", content: prompt }],
       model: "gpt-3.5-turbo",
     });
-    return completion.choices[0];
+    return completion.choices[0].message.content;
   } catch (err) {
     console.error("Failed api call to openai", err);
   }
--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,11 @@
-import { chatCompletion } from "@/ai/openai";
+import { promptAi } from "@/ai/openai";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function GET() {
-  const completion = await chatCompletion(
-    "Could you summarize some news stories from 2021? Answer with a json format containing some useful data related to the news stories."
-  );
-  return NextResponse.json(JSON.parse(completion?.message?.content || ""), {
+export async function POST(request: NextRequest) {
+  const payload = (await request.json()) as { prompt: string }; // TODO verify payload
+  const content = await promptAi(payload.prompt);
+  return NextResponse.json(JSON.parse(content || ""), {
     status: 200,
   });
-}
-
-export async function POST() {
-  return NextResponse.json({ message: "Hello World post" }, { status: 200 });
 }
